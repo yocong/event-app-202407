def32474109befb028f488f50c23cab15cf78073
yocong <rnfaos77@naver.com>
2024-07-11 11:42:48 +0900

    # 인증 폼 생성

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 const Home = () => {
   console.log('home!');
 
+  // Outlet : Home의 children인 homeRouter들을 모두 Outlet링크로 가져오고
+  //          위에 h1태그는 homeRouter에 모두 적용
   return (
     <>
       <h1>My Home Page</h1>
-      <p>
-        <Link to="products">Products</Link>페이지로 이동하기
-      </p>
+      <Outlet />
     </>
   );
 };
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import SignUpForm from '../components/auth/SignUpForm';
+
+const SignUpPage = () => {
+  return <SignUpPage />
+}
+
+export default SignUpPage
